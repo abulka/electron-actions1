efd1d654545cc122b02215efae284ce594dc77e7
Andy Bulka <abulka@gmail.com>
2021-05-20 09:48:55 +1000

    multi file commit message to branch

---
+++ src/Mouth-Collectible.js
@@ -0,0 +1,18 @@
+$(function () {   // document ready
+  // code
+var chart = new OrgChart(document.getElementById("tree"), {
+    nodeBinding: {
+        field_0: "name"
+    },
+    nodes: [
+        { id: 1, name: "Denny Curtis" },
+        { id: 2, pid: 1, name: "Ashley Barnett" },
+        { id: 3, pid: 1, name: "Caden Ellison" },
+        { id: 4, pid: 2, name: "Elliot Patel" },
+        { id: 5, pid: 2, name: "Lynn Hussain" },
+        { id: 6, pid: 3, name: "Tanner May" },
+        { id: 7, pid: 3, name: "Fran Parsons" }
+    ]
+});  
+})
+
